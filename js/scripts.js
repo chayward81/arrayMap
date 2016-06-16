562afae9932f3576dcd95c9c13a5b0bc3dc61445
Charlie Hayward <c.hayward.mail@gmail.com>
2016-06-15 21:43:27 -0700

    Add logic to js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var upSort = function(itemArray) {
+  
+}
+
+
+
+
+
+$(document).ready(function() {
+  $("form#itemList").submit(function() {
+    var items = ["item1","item2","item3","item4","item5","item6","item7","item8","item9","item10"];
+
+    items.forEach(function() {
+        var listInput = $("#item" + item).val();
+
+    }
+  ));
+}
